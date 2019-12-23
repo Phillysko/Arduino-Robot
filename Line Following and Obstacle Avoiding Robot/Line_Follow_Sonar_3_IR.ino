f8ed8600d736721a9ba9098eef1bd96a3438ba0b
philipskopek <48116359+philipskopek@users.noreply.github.com>
2019-12-22 18:08:03 -0800

    Add files via upload

--- Line Following and Obstacle Avoiding Robot/Line_Follow_Sonar_3_IR.ino
+++ Line Following and Obstacle Avoiding Robot/Line_Follow_Sonar_3_IR.ino
@@ -1,7 +1,14 @@
-/*
-  This line following demo uses three of the analog reflective sensors on the Funduino line following sensor
-  to follow a line.
+ /*
+  Philip Skopek
+  Ke Ma
+  
+  ET 387 Lab 3
+  
+  This line following demo uses three IR sensors along with a Sonar to follow a 
+  line and avoid obstacles placed in its path. At the end of the line the robot
+  needs to perform a U-turn and return back to the start.
 */
+
 #include <Servo.h>
 #include <NewPing.h>
 
@@ -18,9 +25,9 @@ Servo servoRight; // Define right servo
 const int lineLSense = A2;
 const int lineMSense = A3;
 const int lineRSense = A4;
-int irReflectL = 0;
-int irReflectM = 0;
-int irReflectR = 0;
+int irL = 0;
+int irM = 0;
+int irR = 0;
 int thresh = 650;
 
 int distance = 100;
@@ -29,21 +36,21 @@ void setup() {
 
   servoLeft.attach(11);
   servoRight.attach(10);
-  
+
   delay(1000);
   distance = readPing();
   delay(100);
   distance = readPing();
- 
+
 }
 void loop() {
 
   // Read reflective sensors
-  irReflectL = analogRead(lineLSense);
-  irReflectM = analogRead(lineMSense);
-  irReflectR = analogRead(lineRSense);
-  distance = readPing();
- 
+  irL = analogRead(lineLSense);
+  irM = analogRead(lineMSense);
+  irR = analogRead(lineRSense);
+  distance = readPing(); // Read sonar distance
+
   if (distance <= 8)
   {
     avoid_obstacle();
@@ -58,52 +65,66 @@ void loop() {
 
 void avoid_obstacle() {
   line_stop();
-  delay(300); //*********************************************Adjust these delays**********
+  delay(300); 
   line_spinLeft();
-  delay(200);     // timing for robot to turn about 60degrees in order to avoid obscatle.
+  delay(1000);     // timing for robot to turn about 60degrees in order to avoid obscatle.
   line_forward();
   delay(1500);
   line_spinRight();
-  delay(300);     // timing for robot to turn about 90 degrees to start moving back towards the line
+  delay(900);     // timing for robot to turn about 90 degrees to start moving back towards the line
   line_forward();
-  delay(700);
+  delay(1800);
+  line_spinRight();
+  delay(500);
+  line_forward();
+
+  while (irL >= thresh) {       // After performing the obstacle avoiding manuever, the robot will travel straight until detecting the line again. We need this while loop
+    irL = analogRead(lineLSense); // so that after detecting the line the robot performs the re-cenerting maneuver
+    irR = analogRead(lineRSense);
+    Serial.println("in the while loop");
+  }
+  line_backwards();       // This is the re-centering maneuver executed after avoiding the obstacle 
+  delay(150):
+  line_spinLeft();
+  delay(500);
+}
+
 }
 
 void follow_line() {
 
-  if (irReflectL <= thresh && irReflectM <= thresh && irReflectR <= thresh) { // B B B
+  if (irL <= thresh && irM <= thresh && irR <= thresh) { // B B B
     line_stop();
     delay(1000);
     line_spinLeft();
     delay(150);
 
   }
-  if (irReflectL <= thresh && irReflectM <= thresh && irReflectR >= thresh) {  // B B W
+  if (irL <= thresh && irM <= thresh && irR >= thresh) {  // B B W
     line_spinRight();
   }
-  if (irReflectL <= thresh && irReflectM >= thresh && irReflectR <= thresh) {  // B W B
+  if (irL <= thresh && irM >= thresh && irR <= thresh) {  // B W B
     line_spinRight();
   }
-  if (irReflectL <= thresh && irReflectM >= thresh && irReflectR >= thresh) { // B W W
+  if (irL <= thresh && irM >= thresh && irR >= thresh) { // B W W
     line_slipLeft();
   }
-  if (irReflectL >= thresh && irReflectM <= thresh && irReflectR <= thresh) { // W B B
+  if (irL >= thresh && irM <= thresh && irR <= thresh) { // W B B
     line_spinLeft();
   }
-  if (irReflectL >= thresh && irReflectM <= thresh && irReflectR >= thresh) { // W B W
+  if (irL >= thresh && irM <= thresh && irR >= thresh) { // W B W
     line_forward();
   }
-  if (irReflectL >= thresh && irReflectM >= thresh && irReflectR <= thresh) { // W W B
+  if (irL >= thresh && irM >= thresh && irR <= thresh) { // W W B
     line_slipRight();
-    delay(150);
   }
-  if (irReflectL >= thresh && irReflectM >= thresh && irReflectR >= thresh) { // W W W
+  if (irL >= thresh && irM >= thresh && irR >= thresh) { // W W W, spin left so that a u-turn is executed at the end of the path
     line_spinLeft();
   }
 
 }
 
-// Sonar Fuctions
+// Sonar Fuctions, returns distace in cm
 int readPing() {
   delay(70);
   int cm = sonar.ping_cm();
